0c6a6c7aada421f1463212d33d71dbd7aeb763bc
Patrick <patrick.chen@gdlink.tw>
2021-12-08 11:39:06 +0800

    arrange model coding keys

--- GithubUsers/Models/PersonalInfoModel.swift
+++ GithubUsers/Models/PersonalInfoModel.swift
@@ -8,69 +8,72 @@
 import Foundation
 
 struct PersonalInfoModel: Codable {
-    
-    let login: String?
-    
-    let id: Int?
-    
-    let node_id: String?
-    
-    let avatar_url: String?
-    
-    let gravatar_id: String?
-    
-    let url: String?
-    
-    let html_url: String?
-    
-    let followers_url: String?
-    
-    let following_url: String?
-    
-    let gists_url: String?
-    
-    let starred_url: String?
-    
-    let subscriptions_url: String?
-    
-    let organizations_url: String?
-    
-    let repos_url: String?
-    
-    let events_url: String?
-    
-    let received_events_url: String?
-    
-    let type: String?
-    
-    let site_admin: Bool?
-    
-    let name: String?
-    
-    let company: String?
-    
-    let blog: String?
-    
-    let location: String?
-    
-    let email: String?
-    
-    let hireable: Bool?
-    
-    let bio: String?
-    
-    let twitter_username: String?
-    
-    let public_repos: Int?
-    
-    let public_gists: Int?
-    
-    let followers: Int?
-    
-    let following: Int?
-    
-    let created_at: String?
-    
-    let updated_at: String?
 
+    let login: String?
+    let id: Int?
+    let nodeID: String?
+    let avatarURL: String?
+    let gravatarID: String?
+    let url: String?
+    let htmlURL: String?
+    let followersURL: String?
+    let followingURL: String?
+    let gistsURL: String?
+    let starredURL: String?
+    let subscriptionsURL: String?
+    let organizationsURL: String?
+    let reposURL: String?
+    let eventsURL: String?
+    let receivedEventsURL: String?
+    let type: String?
+    let siteAdmin: Bool?
+    let name: String?
+    let company: String?
+    let blog: String?
+    let location: String?
+    let email: String?
+    let hireable: Bool?
+    let bio: String?
+    let twitterUsername: String?
+    let publicRepos: Int?
+    let publicGists: Int?
+    let followers: Int?
+    let following: Int?
+    let createdAt: String?
+    let updatedAt: String?
+
+    enum CodingKeys: String, CodingKey {
+        
+        case login
+        case id
+        case nodeID = "node_id"
+        case avatarURL = "avatar_url"
+        case gravatarID = "gravatar_id"
+        case url
+        case htmlURL = "html_url"
+        case followersURL = "followers_url"
+        case followingURL = "following_url"
+        case gistsURL = "gists_url"
+        case starredURL = "starred_url"
+        case subscriptionsURL = "subscriptions_url"
+        case organizationsURL = "organizations_url"
+        case reposURL = "repos_url"
+        case eventsURL = "events_url"
+        case receivedEventsURL = "received_events_url"
+        case type
+        case siteAdmin = "site_admin"
+        case name
+        case company
+        case blog
+        case location
+        case email
+        case hireable
+        case bio
+        case twitterUsername = "twitter_username"
+        case publicRepos = "public_repos"
+        case publicGists = "public_gists"
+        case followers, following
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
 }
