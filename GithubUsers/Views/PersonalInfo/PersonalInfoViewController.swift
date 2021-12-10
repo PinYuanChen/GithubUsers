3cd5d53fa2d99e87e68d9e5275466e7dc86603c5
Patrick <patrick.chen@gdlink.tw>
2021-12-10 11:01:18 +0800

    add follow info and email info to personal info vc

--- GithubUsers/Views/PersonalInfo/PersonalInfoViewController.swift
+++ GithubUsers/Views/PersonalInfo/PersonalInfoViewController.swift
@@ -32,6 +32,10 @@ class PersonalInfoViewController: UIViewController {
     private let profileImageView = UIImageView()
     private let nameLabel = UILabel()
     private let subLabel = UILabel()
+    private let followImageView = UIImageView()
+    private let emailImageView = UIImageView()
+    private let followLabel = UILabel()
+    private let emailLabel = UILabel()
     private let disposeBag = DisposeBag()
 }
 
@@ -46,6 +50,8 @@ private extension PersonalInfoViewController {
         configureProfileImageView()
         configureNameLabel()
         configureSubLabel()
+        configureFollowInfo()
+        configureEmailInfo()
     }
     
     func configureNavigationController() {
@@ -84,13 +90,59 @@ private extension PersonalInfoViewController {
     }
     
     func configureNameLabel() {
+        view.addSubview(nameLabel)
+        nameLabel.snp.makeConstraints {
+            $0.leading.equalTo(profileImageView)
+            $0.top.equalTo(profileImageView.snp.bottom).offset(10)
+        }
         
+        nameLabel.font = .boldSystemFont(ofSize: 20)
     }
     
     func configureSubLabel() {
+        view.addSubview(subLabel)
+        subLabel.snp.makeConstraints {
+            $0.leading.equalTo(nameLabel)
+            $0.top.equalTo(nameLabel.snp.bottom).offset(10)
+        }
         
+        subLabel.font = .systemFont(ofSize: 14)
+        subLabel.textColor = .lightGray
     }
 
+    func configureFollowInfo() {
+        view.addSubview(followImageView)
+        followImageView.snp.makeConstraints {
+            $0.size.equalTo(20)
+            $0.leading.equalTo(subLabel)
+            $0.top.equalTo(subLabel.snp.bottom).offset(10)
+        }
+        followImageView.image = UIImage(named: "people")
+        
+        view.addSubview(followLabel)
+        followLabel.snp.makeConstraints {
+            $0.centerY.equalTo(followImageView)
+            $0.leading.equalTo(followImageView.snp.trailing).offset(10)
+        }
+        followLabel.textColor = .lightGray
+    }
+    
+    func configureEmailInfo() {
+        view.addSubview(emailImageView)
+        emailImageView.snp.makeConstraints {
+            $0.size.equalTo(20)
+            $0.leading.equalTo(followImageView)
+            $0.top.equalTo(followImageView.snp.bottom).offset(10)
+        }
+        emailImageView.image = UIImage(named: "email")
+        
+        view.addSubview(emailLabel)
+        emailLabel.snp.makeConstraints {
+            $0.centerY.equalTo(emailImageView)
+            $0.leading.equalTo(emailImageView.snp.trailing).offset(10)
+        }
+        emailLabel.textColor = .lightGray
+    }
 }
 
 // MARK: - Private func
@@ -111,6 +163,10 @@ private extension PersonalInfoViewController {
                 guard let self = self,
                       let imgURL = model.avatarURL else { return }
                 self.profileImageView.kf.setImage(with: URL(string: imgURL))
+                self.nameLabel.text = model.name
+                self.subLabel.text = model.login
+                self.followLabel.text = "\(model.followers ?? 0) followers, \(model.following ?? 0) following"
+                self.emailLabel.text = model.email
             })
             .disposed(by: disposeBag)
     }
